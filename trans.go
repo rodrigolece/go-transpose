5392b4e3e5ce4b4c51cd14a3ece2195ee0183e30
Rodrigo Leal Cervantes <rodrigolealc@ciencias.unam.mx>
2016-01-13 12:59:51 -0600

    Agregué tipo dense y método .tanspose()

--- trans.go
+++ trans.go
@@ -5,7 +5,11 @@ import (
     "github.com/rodrigolece/transpose/matutil"
 )
 
-func transpose(A mat64.Matrix) mat64.Matrix{
+type dense struct {
+    *mat64.Dense
+}
+
+func transpose(A *mat64.Dense) *mat64.Dense {
     r, s := A.Dims()
     var data []float64
 
@@ -15,34 +19,31 @@ func transpose(A mat64.Matrix) mat64.Matrix{
         }
     }
 
-    // Está medio chafa que regrese Dense, cómo hacemos para que regrese
-    // el mismo tipo de A?
     return mat64.NewDense(s, r, data)
 }
 
-// cannot define new methods on non-local type mat64.Dense
-// func (A *mat64.Dense) transpose() {
-//     A.Copy(transpose(A))
-// }
+func (A *dense) transpose() {
+    // A.Copy(transpose(A))
+    r, s := A.Dims()
+    var data []float64
+
+    for  j := 0 ; j < s ; j++ {
+        for i := 0 ; i < r ; i++ {
+            data = append(data, A.At(i,j))
+        }
+    }
+
+    *A = dense{mat64.NewDense(s, r, data)}
+}
 
 
 func main() {
     A := mat64.NewDense(2,3, []float64{0,1,0,0,2,3})
-    matutil.PrintMat(A)
+    // matutil.PrintMat(A)
 
-    matutil.PrintMat(transpose(A))
+    B := dense{A} // no es necesario &dense{A}
+    matutil.PrintMat(B)
+
+    B.transpose()
+    matutil.PrintMat(B)
 }
-
-
-/* Dos diferencias con Julia me molestan un poco:
- - No se pueden definir nuevos métodos en tipos importados (no-locales)
- - No sé como regresar una transpuesta que sea del mismo tipo que la matriz
- con la que se llama a transpose. Hace falta algo del tipo de similar()
-
- Vale la pena buscar el equivalente de show(), que aquí sería cómo extender
- los métodos de print para lo que me sirve a mí. (Valdría la pena asegurarme
- de que los prints son la única manera de interactuar con el mundo exterior.
-
- Fuera de las desventajas mencionadas, es muy fácil implementar el algoritmo
- de reescribir las columnas de A como renglones, porque basta llenar data en el
- orden correcto. */
